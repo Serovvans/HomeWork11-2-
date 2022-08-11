ae8c7732b05ed80ccd3f3017201c1d6d10360c0b
Serovvans <serovvans@yandex.ru>
2022-08-11 14:39:56 +0300

    complete flask app

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+from utils import *
+from flask import Flask, render_template
 
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def page_index():
+    """Представление списка всех кандидатов"""
+    candidates = load_candidates_from_json()
+    return render_template("list.html", candidates=candidates)
+
+
+@app.route("/candidate/<int:candidate_id>")
+def page_candidate(candidate_id: int):
+    """Представление информации об одном кандидате"""
+    candidate = get_candidate(candidate_id)
+    return render_template("single.html", candidate=candidate)
+
+
+@app.route("/search/<candidate_name>")
+def page_search(candidate_name: str):
+    """Представление поиска кандидатов по имени"""
+    candidates = get_candidates_by_name(candidate_name)
+    return render_template("search.html", candidates=candidates, count=len(candidates))
+
+
+@app.route("/skill/<skill_name>")
+def page_skill(skill_name: str):
+    """Представление поиска кандидатов по скиллу"""
+    candidates = get_candidates_by_skill(skill_name)
+    return render_template("skill.html", skill=skill_name, count=len(candidates), candidates=candidates)
+
+
+if __name__ == "__main__":
+    app.run()
--- utils.py
+++ utils.py
@@ -1,4 +1,43 @@
 import os
 import json
 
-from typing import List
+from typing import List, Dict
+
+
+def load_candidates_from_json() -> List[Dict]:
+    """Возвращает список всех кандидатов"""
+    file = os.path.join("data", "candidates.json")
+
+    with open(file, encoding="UTF-8") as data:
+        candidates = json.load(data)
+    return candidates
+
+
+def get_candidate(candidate_id: int) -> Dict:
+    """Возвращает одного кандидата по его id"""
+    candidates = load_candidates_from_json()
+    for candidate in candidates:
+        if candidate["id"] == candidate_id:
+            return candidate
+
+
+def get_candidates_by_name(candidate_name: str) -> List[Dict]:
+    """Возвращает кандидатов по имени"""
+    candidates = load_candidates_from_json()
+
+    candidates_list = []
+    for candidate in candidates:
+        if candidate_name in candidate["name"].split():
+            candidates_list.append(candidate)
+    return candidates_list
+
+
+def get_candidates_by_skill(skill_name: str) -> List[Dict]:
+    """Возвращает кандидатов по навыку"""
+    candidates = load_candidates_from_json()
+
+    candidates_list = []
+    for candidate in candidates:
+        if skill_name.lower() in candidate["skills"].lower().split(", "):
+            candidates_list.append(candidate)
+    return candidates_list
